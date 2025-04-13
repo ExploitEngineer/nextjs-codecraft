4d5cbd978545372d08f866aa3369673fad00e07a
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-13 21:16:32 +0500

    feat: add routing in next/js

---
+++ next-app/src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center">
+            <h1 className="font-semibold font-mono text-white text-2xl">My Profile Page</h1>
+        </div>
+    );
+};
