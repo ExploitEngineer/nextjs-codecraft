c376ae5360e3e69f78e8e3aea053b37c1327fa0d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-15 00:05:42 +0500

    feat: add not-found.tsx file and notFound function by next.js

--- next-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ next-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({ params }: {
     params: {
         productId: string;
         reviewId: string;
     };
 }) {
+    if (parseInt(params.reviewId) > 100) {
+        notFound();
+    }
     return (
         <div className="w-full min-h-screen flex items-center justify-center">
             <h1 className="font-semibold font-mono text-white text-2xl">Review {params.reviewId} for product {params.productId}</h1>
