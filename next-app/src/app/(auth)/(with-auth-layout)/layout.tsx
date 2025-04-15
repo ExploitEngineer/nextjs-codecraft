0aecc589167378ebd50d88cca31473b9c7aba020
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-16 01:03:24 +0500

    feat: add active links in next.js

--- next-app/src/app/(auth)/(with-auth-layout)/layout.tsx
+++ next-app/src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,10 +1,32 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const navLinks = [
+    { name: 'Register', href: '/register' },
+    { name: "Login", href: '/login' }
+];
+
 export default function AuthLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+
+    const pathname = usePathname();
+
     return (
         <div>
+            <div>
+                {
+                    navLinks.map((link) => {
+                        const isActive = pathname.startsWith(link.href)
+                        return (
+                            <Link className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"} href={link.href} key={link.name}>{link.name}</Link>
+                        )
+                    })
+                }
+            </div>
             <h2>Inner Layout</h2>
             {children}
         </div>
