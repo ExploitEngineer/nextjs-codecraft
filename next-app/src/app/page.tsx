f58443ac5acb904fecdd254f3612e018dbea00bd
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-16 00:50:28 +0500

    feat: add Link component Navigation in next.js

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <div className="w-full min-h-screen flex items-center justify-center">
+        <div className="w-full min-h-screen flex flex-col gap-10 items-center justify-center">
             <h1 className="font-semibold font-mono text-white text-2xl">Home Page</h1>
+            <div className="flex gap-3">
+                <Link className="px-6 py-2 font-semibold bg-sky-600 rounded-lg" href='/blog'>Blog</Link>
+                <Link className="px-6 py-2 font-semibold bg-sky-600 rounded-lg" href='/products'>Products</Link>
+            </div>
         </div>
     );
 };
--- next-app/src/app/products/page.tsx
+++ next-app/src/app/products/page.tsx
@@ -1,12 +1,19 @@
+import Link from 'next/link'
+
 export default function ProductList() {
+
+    const productId = 100;
+
     return (
-        <div className="w-full min-h-screen flex items-center justify-center font-mono font-medium text-white">
-            <div className="text-center">
+        <div className="w-full min-h-screen flex flex-col gap-10 items-center justify-center font-mono font-medium text-white">
+            <div className="text-center space-y-5">
                 <h1 className="font-semibold text-white text-2xl mb-4">Product List</h1>
-                <h3>Product 1</h3>
-                <h3>Product 2</h3>
-                <h3>Product 3</h3>
+                <h3><Link className="px-6 py-2 font-semibold bg-green-600 rounded-lg" href='/products/1'>Product 1</Link></h3>
+                <h3><Link className="px-6 py-2 font-semibold bg-green-600 rounded-lg" href='/products/2'>Product 2</Link></h3>
+                <h3><Link className="px-6 py-2 font-semibold bg-green-600 rounded-lg" href='/products/3' replace>Product 3</Link></h3>
+                <h3><Link className="px-6 py-2 font-semibold bg-green-600 rounded-lg" href={`products/${productId}`}>Product {productId}</Link></h3>
             </div>
+            <Link className="px-6 py-2 font-semibold bg-sky-600 rounded-lg" href='/'>Home</Link>
         </div>
     );
 };
