74baa2069f5abf9c7e61c2edf0da9e9fa5f05590
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-16 00:15:06 +0500

    feat: add static and dynamic routing metadata in next.js

--- next-app/src/app/about/page.tsx
+++ next-app/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "About Codevolution"
+};
+
 export default function About() {
     return (
         <div className="w-full min-h-screen flex items-center justify-center">
