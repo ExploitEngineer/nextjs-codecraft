d589867629b0563927ee08d32a1196a3761dfe91
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-29 02:13:52 +0500

    feat: add nested dynamic routes in next.js

---
+++ next_v15/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,12 @@
+export default async function ProductReview({ params }: {
+    params: Promise<{ productId: string; reviewId: string }>;
+}) {
+
+    const { productId, reviewId } = await params;
+
+    return (
+        <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
+            <h1 className="text-white font-semibold text-2xl">Review {reviewId} for product {productId}</h1>
+        </div>
+    );
+}
