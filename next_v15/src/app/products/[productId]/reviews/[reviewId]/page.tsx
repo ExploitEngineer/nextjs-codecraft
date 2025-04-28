e6cd775bfc613d5812825e3bff80be394c9bc0e5
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-29 02:51:22 +0500

    feat: add not-found page, notFound function, usePathname, each not-found for each page in next.js

--- next_v15/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ next_v15/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductReview({ params }: {
     params: Promise<{ productId: string; reviewId: string }>;
 }) {
 
     const { productId, reviewId } = await params;
+
+    if (parseInt(reviewId) > 1000) {
+        notFound();
+    }
 
     return (
         <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
