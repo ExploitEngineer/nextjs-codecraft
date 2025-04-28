9446fd635db6be8706e35af56568d220f4e40bce
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-29 01:59:31 +0500

    feat: add nested routes in next.js

---
+++ next_v15/src/app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+export default function SecondBlog() {
+    return (
+        <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
+            <h1 className="text-white font-semibold text-2xl">Second blog post</h1>
+        </div>
+    );
+}
