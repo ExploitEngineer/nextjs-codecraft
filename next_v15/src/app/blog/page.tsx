d626c14eb29cb14cffe0f6b6057e6367f4bb2e7b
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-30 00:03:06 +0500

    feat: add title metadata in next.js

--- next_v15/src/app/blog/page.tsx
+++ next_v15/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog"
+    }
+};
+
 export default function Blog() {
     return (
         <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
