d2224cfcea89791b94133c1820d081ee66e3d1d4
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-30 00:49:44 +0500

    feat: add Link components in next.js

--- next_v15/src/app/page.tsx
+++ next_v15/src/app/page.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
     return (
         <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
-            <h1 className="text-white font-semibold text-2xl">Welcome home!</h1>
+            <div className="flex flex-col space-y-4">
+                <h1 className="text-white font-semibold text-2xl">Welcome home!</h1>
+                <div>
+                    <Link href='/blog'>
+                        <Button className="bg-zinc-700 py-6 w-full font-semibold text-lg">Blog</Button>
+                    </Link>
+                </div>
+                <div>
+                    <Link href='/products'>
+                        <Button className="bg-zinc-700 py-6 w-full font-semibold text-lg">Products</Button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
