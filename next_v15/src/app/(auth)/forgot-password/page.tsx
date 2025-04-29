4757b4d718de7d5261595a651bc679bb30321a3f
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-29 11:12:26 +0500

    feat: add route groups in next.js

---
+++ next_v15/src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+    return (
+        <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
+            <h1 className="text-white font-semibold text-2xl">Forgot Password</h1>
+        </div>
+    );
+}
---
+++ next_v15/src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
+            <h1 className="text-white font-semibold text-2xl">Login</h1>
+        </div>
+    );
+}
