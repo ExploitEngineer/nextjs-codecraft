0ae9855d84461d6934409d859c32fad623542ce4
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-30 10:49:17 +0500

    feat: add loading.tsx file in next.js

--- next_v15/src/app/blog/page.tsx
+++ next_v15/src/app/blog/page.tsx
@@ -6,7 +6,14 @@ export const metadata: Metadata = {
     }
 };
 
-export default function Blog() {
+export default async function Blog() {
+
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("international delay");
+        }, 2000)
+    })
+
     return (
         <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
             <h1 className="text-white font-semibold text-2xl">My blog</h1>
