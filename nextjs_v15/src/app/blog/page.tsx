e4d5394aaaf94d3d2656cfae32fcb58ed45aa3a7
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-05 02:36:28 +0500

    feat: add title metadata
    - Setting title with a string
    - Advance title control with objects
    - Using default title property
    - Template option for consistent titles
    - Absolute title for custom titles

--- nextjs_v15/src/app/blog/page.tsx
+++ nextjs_v15/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const meatadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
+};
+
 export default function Blog() {
   return <h1>My blog</h1>;
 }
