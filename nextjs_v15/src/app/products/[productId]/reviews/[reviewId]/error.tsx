6ae38219fbcc94f17ad49b44544c17e92715a66d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-06 02:20:31 +0500

    feat: add recovering from errors
    - Implemented the reset function
    - added a retry button
    - client-side retry limitations
    - server-side recovery with react and next.js
    - implemented the reload function

--- nextjs_v15/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ nextjs_v15/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,32 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
-    <div className="text-red-600 font-semibold text-sm">{error.message}</div>
+    <div className="flex flex-col gap-2 items-start">
+      <p className="text-red-600 font-semibold text-sm">{error.message}</p>
+      <button
+        className="bg-orange-600 text-white font-semibold font-mono text-sm py-2 px-8 cursor-pointer rounded-lg"
+        onClick={reload}
+      >
+        Try again
+      </button>
+    </div>
   );
 }
