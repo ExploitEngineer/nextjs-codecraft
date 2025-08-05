9b0d10d143d091b4e90afc212c8f405187f7ced4
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-05 23:25:23 +0500

    feat: add Link component
    - Implemented client-side navigation with Link component
    - Added links to navigate between pages
    - Dynamic links with template literals
    - replace prop in Link component

--- nextjs_v15/src/app/page.tsx
+++ nextjs_v15/src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome home!</h1>;
+  return (
+    <>
+      <h1>Welcome home!</h1>
+      <div className="flex font-mono text-sm gap-2">
+        <Link className="underline text-blue-600" href="/blog">
+          Blog
+        </Link>
+        <Link className="underline text-blue-600" href="/products">
+          Products
+        </Link>
+      </div>
+    </>
+  );
 }
--- nextjs_v15/src/app/products/page.tsx
+++ nextjs_v15/src/app/products/page.tsx
@@ -1,10 +1,28 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const productId: number = 100;
+
   return (
     <>
+      <Link className="underline text-blue-600 font-mono text-sm" href="/">
+        home
+      </Link>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/products/${productId}`}>Product {productId}</Link>
+      </h2>
     </>
   );
 }
