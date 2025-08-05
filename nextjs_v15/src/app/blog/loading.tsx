e81f9601965b394507b07396fce8874d5a945f77
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-06 01:55:44 +0500

    feat: add Loading file
    - created the loading state
    - Implemented the delay to test the loading
    - Implemented the loading.tsx file

---
+++ nextjs_v15/src/app/blog/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="w-full min-h-screen bg-zin-800 flex items-center justify-center">
+      <h1 className="animate-pulse text-white font-semibold font-mono text-shadow-lg text-shadow-blue-600">
+        Loading
+      </h1>
+    </div>
+  );
+}
