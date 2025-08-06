8bb2ae38902ff1d62ed51a3c970a4f76aa4dd9f1
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-07 02:08:28 +0500

    feat: add handling PATCH requests
    - implemented the PATCH request handler
    - update the comment text

--- nextjs_v15/src/app/comments/[id]/route.ts
+++ nextjs_v15/src/app/comments/[id]/route.ts
@@ -14,3 +14,22 @@ export async function GET(
 
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  console.log("Body:", body);
+  const { text } = body;
+
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  if (index === -1) {
+    return Response.json({
+      message: "no comment with this id to perform this task",
+    });
+  }
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
