6e42d1ae551e627d0760d204191d263e2cdf671a
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-07 01:37:48 +0500

    feat: add handling GET requests
    - create the sample data
    - implemented the GET route handler

---
+++ nextjs_v15/src/app/comments/data.ts
@@ -0,0 +1,10 @@
+interface CommentsType {
+  id: number;
+  text: string;
+}
+
+export const comments: CommentsType[] = [
+  { id: 1, text: "This is the first comment" },
+  { id: 2, text: "This is the second comment" },
+  { id: 3, text: "This is the third comment" },
+];
